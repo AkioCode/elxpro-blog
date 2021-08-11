1a751165c69e300866b6cff00aa6d9b32195b40e
Rodrigo Otsuka <akio2631@gmail.com>
2021-08-11 02:08:23 +0000

    🔧 Config credo and adjust credo errors

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -16,6 +16,7 @@ defmodule ElxproBlogWeb.ChannelCase do
   """
 
   use ExUnit.CaseTemplate
+  alias Ecto.Adapters.SQL.Sandbox
 
   using do
     quote do
@@ -29,10 +30,10 @@ defmodule ElxproBlogWeb.ChannelCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(ElxproBlog.Repo)
+    :ok = Sandbox.checkout(ElxproBlog.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(ElxproBlog.Repo, {:shared, self()})
+      Sandbox.mode(ElxproBlog.Repo, {:shared, self()})
     end
 
     :ok
