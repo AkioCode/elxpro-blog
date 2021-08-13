385cf8196469cc3174ed295864df21607f3891a6
Rodrigo Otsuka <akio2631@gmail.com>
2021-08-13 03:01:24 +0000

    feat: add floating action button in index oposts

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,8 +17,5 @@ import "phoenix_html"
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.fixed-action-btn');
   M.FloatingActionButton.init(elems, {});
-
-  var elems = document.querySelectorAll('.sidenav');
-  M.Sidenav.init(elems, {});
 });
 
