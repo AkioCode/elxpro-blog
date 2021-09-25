7922b62b02305ecdc5f962c46b2fea5ded03d72c
Rodrigo Otsuka <akio2631@gmail.com>
2021-09-25 06:44:57 +0000

    ✨ Add broadcasting when commenting

--- lib/elxpro_blog_web/channels/comments_channel.ex
+++ lib/elxpro_blog_web/channels/comments_channel.ex
@@ -1,24 +1,33 @@
 defmodule ElxproBlogWeb.CommentsChannel do
   use ElxproBlogWeb, :channel
 
+  alias ElxproBlog.Posts.Core.PostRepo
+  alias ElxproBlog.Comments.Core.CommentRepo
+
   @impl true
   def join("comments:"<>post_id, payload, socket) do
-    post = ElxproBlog.Posts.Core.PostRepo.get_post_with_comments!(post_id)
-    {:ok, %{comments: post.comments}, socket}
-  end
-
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+    post = PostRepo.get_post_with_comments!(post_id)
+    {:ok, %{comments: post.comments}, assign(socket, :post_id, post_id)}
   end
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (comments:lobby).
   @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
+  def handle_in("comment:add", payload, socket) do
+    response =
+      socket.assigns.post_id
+      |> CommentRepo.create_comment(%{content: payload})
+
+    case response do
+      {:ok, comment} ->
+
+        broadcast!(socket, "comments:#{socket.assigns.post_id}:new", %{comment: comment})
+        {:reply, :ok, socket}
+
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}}
+    end
+
+    {:reply, :ok, socket}
   end
 end
